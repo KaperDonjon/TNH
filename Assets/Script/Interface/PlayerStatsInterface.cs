474659ed7729c8314a131bac196b508ad6c76bfe
Ha4iko <Kazlikhin@mail.ru>
2019-04-06 04:02:39 +0300

    Ubdate Skill point

--- Assets/Script/Interface/PlayerStatsInterface.cs
+++ Assets/Script/Interface/PlayerStatsInterface.cs
@@ -11,15 +11,28 @@ public class PlayerStatsInterface : MonoBehaviour
     public int LvL;
     public int SkillPoint;
 
-    public int HPpoint = 1;
-    public int StaminaPoint = 1;
-    public int ManaPoint = 1;
-    public int ResistPoint = 1;
+    public int HPpoint;
+    public int StaminaPoint ;
+    public int ManaPoint ;
+    public int ResistPoint;
 
     public int THPpoint;
     public int TStaminaPoint;
     public int TManaPoint;
     public int TResistPoint;
+
+    public int Fire;
+    public int Woter;
+    public int Nature;
+
+    public int TFire;
+    public int TWoter;
+    public int TNature;
+
+    public int TMaxHP;
+    public int TMaxMana;
+    public int TMaxStamina;
+    public int TResist;
 
     public int TLvL = 5;
     public int TSkillPoint;
@@ -44,8 +57,9 @@ public class PlayerStatsInterface : MonoBehaviour
 
         if (TLvL != LvL)
         {
-            TSkillPoint = (TLvL - LvL) * 2;
+            TSkillPoint += 5;
             LvL = TLvL;
+            SkillPoint = TSkillPoint;
         }
 
 
@@ -64,7 +78,16 @@ public class PlayerStatsInterface : MonoBehaviour
             GUI.Label(new Rect(Screen.width / 2 - 80, Screen.width / 2 - 330, 200, 100), "Стамина");
             GUI.Label(new Rect(Screen.width / 2 - 80, Screen.width / 2 - 310, 200, 100), "Мана");
             GUI.Label(new Rect(Screen.width / 2 - 80, Screen.width / 2 - 290, 200, 100), "Резисты");
-            GUI.Label(new Rect(Screen.width / 2 - 80, Screen.width / 2 - 250, 200, 100), TSkillPoint.ToString());
+            GUI.Label(new Rect(Screen.width / 2 - 80, Screen.width / 2 - 130, 200, 100), "СкилПоинты:");
+            GUI.Label(new Rect(Screen.width / 2, Screen.width / 2 - 130, 200, 100), TSkillPoint.ToString());
+
+            GUI.Label(new Rect(Screen.width / 2 - 80, Screen.width / 2 - 250, 200, 100), "Огонь:");
+            GUI.Label(new Rect(Screen.width / 2 - 80, Screen.width / 2 - 230, 200, 100), "Вода:");
+            GUI.Label(new Rect(Screen.width / 2, Screen.width / 2 - 250, 200, 100), "Природа:");
+
+            GUI.Label(new Rect(Screen.width / 2 - 30, Screen.width / 2 - 250, 200, 100), Fire.ToString());
+            GUI.Label(new Rect(Screen.width / 2 - 30, Screen.width / 2 - 230, 200, 100), Woter.ToString());
+            GUI.Label(new Rect(Screen.width / 2 + 60, Screen.width / 2 - 250, 200, 100), Nature.ToString());
 
             GUI.Label(new Rect(Screen.width / 2 + 30, Screen.width / 2 - 350, 15, 15), HPpoint.ToString());
             GUI.Label(new Rect(Screen.width / 2 + 30, Screen.width / 2 - 330, 15, 15), StaminaPoint.ToString());
@@ -149,6 +172,10 @@ public class PlayerStatsInterface : MonoBehaviour
                     TSkillPoint -= 1;
                     ResistPoint += 1;
                     ps.Resist = ps.Resist + 2;
+                    Fire += 10;
+                    Woter += 10;
+                    Nature += 10;
+
                 }
 
             }
@@ -159,11 +186,77 @@ public class PlayerStatsInterface : MonoBehaviour
                     TSkillPoint += 1;
                     ResistPoint -= 1;
                     ps.Resist = ps.Resist - 2;
+                    Fire -= 10;
+                    Woter -= 10;
+                    Nature -= 10;
                 }
 
             }
 
+
+
+
+
+
+
+
+            //Принять
+            if (GUI.Button(new Rect(Screen.width / 2 + 60, Screen.width / 2 - 150, 60, 20), "Принять"))
+            {
+                SkillPoint = TSkillPoint;
+                THPpoint = HPpoint;
+                TStaminaPoint = StaminaPoint;
+                TManaPoint = ManaPoint;
+                TResistPoint = ResistPoint;
+                TFire = Fire;
+                TWoter = Woter;
+                TNature = Nature;
+
+            }
+            //Отмена
+            if (GUI.Button(new Rect(Screen.width / 2 + 130, Screen.width / 2 - 150, 75, 20), "Отменить"))
+            {
+                HPpoint = THPpoint;
+                StaminaPoint = TStaminaPoint;
+                ManaPoint = TManaPoint;
+                ResistPoint = TResistPoint;
+                TSkillPoint = SkillPoint;
+
+                Fire = TFire;
+                Woter = TWoter;
+                Nature = TNature;
+
+                ps.MaxHP = TMaxHP; 
+                ps.MaxHP = TMaxHP;
+                ps.MaxMana = TMaxMana;
+                ps.MaxStamina = TMaxStamina;
+                ps.Resist = TResist;
+            }
+           
+
+
+
+
+
+
+                //Выход
+                if (GUI.Button(new Rect(Screen.width / 2 + 150, Screen.width / 2 - 120, 50, 25), "Выйти"))
+            {
+                Visible = false;
+            }
+        }
+        //ЛвЛ
+         GUI.Label(new Rect(Screen.width / 2, 20 , 400, 400), TLvL.ToString());
+
+
+        //Лвл ап
+        if (GUI.Button(new Rect(Screen.width / 2 + 250, Screen.width / 2 - 150, 75, 20), "+LVL"))
+        {
+            TLvL += 1;
         }
 
     }
+
+   
+
 }
